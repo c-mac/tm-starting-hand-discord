17b16e776c4a31b67f0d7c2b457a158be0c1aa64
Cameron McCallie <mccallie.cameron@gmail.com>
2025-06-27 08:59:33 -0700

    colonies, post to one channel

--- creds.example.py
+++ creds.example.py
@@ -1,3 +1,2 @@
 BASE_CHANNEL= # (int) right click channel name in developer mode in discord to get this id
-VENUS_CHANNEL= # (int) right click channel name in developer mode in discord to get this id
 DISCORD_TOKEN= # (string) Discord bot token /applications/bot/<id> -> Token -> Copy
--- server.py
+++ server.py
@@ -3,12 +3,13 @@ import modal
 import creds
 
 
-def gen_card_urls():
+def gen_card_urls(format: str):
     url = "https://ssimeonoff.github.io/cards-list"
-    preludes = [f"P0{i+1}" if i < 9 else f"P{i+1}" for i in range(35)]
+    preludes = [f"P0{i + 1}" if i < 9 else f"P{i + 1}" for i in range(35)]
 
     cards = [
-        f"{i+1}" if i >= 99 else f"0{i+1}" if i >= 9 else f"00{i+1}" for i in range(208)
+        f"{i + 1}" if i >= 99 else f"0{i + 1}" if i >= 9 else f"00{i + 1}"
+        for i in range(208)
     ]
 
     corps = {
@@ -39,39 +40,75 @@ def gen_card_urls():
         "CORP17": "Viron",
     }
 
-    merged_corps = {**corps, **venus_corps}
+    promo_corps = {
+        "CORP36": "Arcadian Communities",
+        "CORP37": "Recyclon",
+        "CORP38": "Splice",
+        "CORP39": "Astrodrill",
+        "CORP40": "Pharmacy Union",
+    }
+
+    colonies_corps = {
+        "CORP23": "Aridor",
+        "CORP24": "Arklight",
+        "CORP25": "Polyphemos",
+        "CORP26": "Poseidon",
+        "CORP27": "Storm Craft",
+    }
+
+    final_corps = corps
 
     prelude_cards = ["P36", "P37", "P38", "P39", "P40", "P41", "P42"]
 
     venus_cards = [f"{i}" for i in range(213, 262)]
 
+    # X14 - X30 (31,32,33 are replacements of base cards)
+    promo_cards = ["209", "210", "211", "212", *[f"X{i}" for i in range(14, 31)]]
+
+    colonies_cards = [f"C{i}" if i > 9 else f"C0{i}" for i in range(50)]
+
+    colonies = [
+        "EUROPA",
+        "TRITON",
+        "CALLISTO",
+        "CERES",
+        "ENCELADUS",
+        "TITAN",
+        "MIRANDA",
+        "GANYMEDE",
+        "LUNA",
+        "IO",
+        "PLUTO",
+    ]
+
     final_cards = cards + prelude_cards
+    corp_choices = 2
+    if format == "BPVC":
+        # Remove deimos, mag field generators, and great dam -- replace with their promo equivalents below
+        cards.remove("039")
+        cards.remove("165")
+        cards.remove("136")
+        promo_cards += ["X31", "X32", "X33"]
+        final_cards += promo_cards + colonies_cards + venus_cards
+        final_corps = {**corps, **venus_corps, **promo_corps, **colonies_corps}
 
     choices = (
-        random.sample(list(corps), 2)
+        random.sample(list(final_corps), corp_choices)
         + random.sample(preludes, 4)
         + random.sample(final_cards, 10)
     )
 
-    venus_choices = (
-        random.sample(list(merged_corps), 2)
-        + random.sample(preludes, 4)
-        + random.sample(final_cards + venus_cards, 10)
-    )
+    if format == "BPVC":
+        choices += random.sample(colonies, 5)
 
-    final_url = f'{url}#{"#".join(choices)}'
-    venus_url = f'{url}#{"#".join(venus_choices)}'
+    final_url = f"{url}#{'#'.join(choices)}"
 
     return {
-        "base": final_url,
-        "venus": venus_url,
-        "base_corps": list(
-            map(lambda x: corps.get(x), [x for x in choices if x.startswith("CORP")])
-        ),
-        "venus_corps": list(
+        "url": final_url,
+        "corps": list(
             map(
-                lambda x: merged_corps.get(x),
-                [x for x in venus_choices if x.startswith("CORP")],
+                lambda x: final_corps.get(x),
+                [x for x in choices if x.startswith("CORP")],
             )
         ),
     }
@@ -92,22 +129,23 @@ def run_bot():
     @client.event
     async def on_ready():
         base_channel = client.get_channel(creds.BASE_CHANNEL)
-        venus_channel = client.get_channel(creds.VENUS_CHANNEL)
 
-        data = gen_card_urls()
+        format = random.choice(["BP", "BPVC"])
+
+        data = gen_card_urls(format)
 
         board = random.sample(["Tharsis", "Elysium", "Hellas"], 1)
         order = random.sample(["first", "second"], 1)
 
-        venus_message = f'{data["venus_corps"][1]} vs. {data["venus_corps"][0]} on {board[0]}, going {order[0]}'
-        base_message = f'{data["base_corps"][1]} vs. {data["base_corps"][0]} on {board[0]}, going {order[0]}'
+        base_message = f"{format}: {data['corps'][1]} vs. {data['corps'][0]} on {board[0]}, going {order[0]}"
 
-        await base_channel.create_thread(name=base_message, content=data["base"])
-
-        await venus_channel.create_thread(name=venus_message, content=data["venus"])
+        await base_channel.create_thread(name=base_message, content=data["url"])
 
     client.run(creds.DISCORD_TOKEN)
 
 
 if __name__ == "__main__":
     app.deploy("tm-hand-bot")
+    # format = random.choice(["BP", "BPVC"])
+    # data = gen_card_urls(format)
+    # print(data["url"])
